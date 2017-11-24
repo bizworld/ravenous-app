e965162fd6b845a5da0964c48127cbcb99493501
bizworld <bizworld@outlook.com>
2017-11-23 21:16:36 -0500

    Import the <Business /> component (that will be produced by the Business component class).

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,2 +1,3 @@
 import React from 'react'; // import the React library
-import './BusinessList.css'; // import BusinessList.css (located in the same parent folder)
+import './BusinessList.css'; // import BusinessList.css (located in the same folder)
+import Business from '../Business/Business'; // import the Buiness component class (.. is a parent directory)
