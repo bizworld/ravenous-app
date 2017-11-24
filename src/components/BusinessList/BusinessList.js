8dd6448867ed4867334ad108428ec624237c3526
bizworld <bizworld@outlook.com>
2017-11-23 21:31:28 -0500

    Create the <BusinessList /> component (by 1st creating the BusinessList component class).

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,3 +1,15 @@
 import React from 'react'; // import the React library
 import './BusinessList.css'; // import BusinessList.css (located in the same folder)
 import Business from '../Business/Business'; // import the Buiness component class (.. is a parent directory)
+
+/* The point of the <BusinessList /> component is to simulate what a returned
+list of businesses would look like in Ravenous (after querying e.g. the Yelp API).
+To help this simulation, <BusinessList /> will make use of the
+<Business /> component repeatedly.
+Create the BusinessList component class (that will produce the <BusinessList />
+component). */
+class BusinessList extends React.Component {
+  render() {
+    
+  }
+}
