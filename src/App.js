bbb51364e63093bee500ce1100e50d1eabc7626a
bizworld <bizworld@outlook.com>
2017-11-25 11:38:41 -0500

    Render Ravenous.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'components/SearchBar/SearchBar.js';
+import 'components/BusinessList/BusinessList.js';
 
 class App extends Component {
   render() {
     // We won't need the default App component, so we'll make some modifications
     return (
-
+      div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
     );
   }
 }
