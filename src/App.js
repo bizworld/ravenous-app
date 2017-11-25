163d43de0b739f09c2f1777449a95255bee7c80a
bizworld <bizworld@outlook.com>
2017-11-25 11:23:23 -0500

    Modify the default <App /> component (in the src folder).

--- src/App.js
+++ src/App.js
@@ -4,16 +4,9 @@ import './App.css';
 
 class App extends Component {
   render() {
+    // We won't need the default App component, so we'll make some modifications
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
     );
   }
 }
