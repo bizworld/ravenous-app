23cafdf64cc22951bed21f399cdc276ea4cd136d
bizworld <bizworld@outlook.com>
2017-12-04 21:01:39 -0500

    Handle a search (onClick), by using handleSearch().

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -128,7 +128,7 @@ class SearchBar extends React.Component {
           <input placeholder="Where?" onChange={this.handleLocationChange}/>
         </div>
         <div className="SearchBar-submit">
-          <a>{`Let's Go`}</a>
+          <a onClick={this.handleSearch}>{`Let's Go`}</a>  {/* use handleSearch() */}
         </div>
       </div>
     );
