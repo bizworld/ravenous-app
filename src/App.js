2b4ea0ad3b360b6281e9cc4c115edebe78dd5a27
bizworld <bizworld@outlook.com>
2017-12-04 20:10:48 -0500

    Set the searchYelp prop in SearchBar.

--- src/App.js
+++ src/App.js
@@ -47,7 +47,8 @@ class App extends Component {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
+        {/* use the searchYelp() method. By adding a searchYelp property to the SearchBar component */}
+        <SearchBar searchYelp={this.searchYelp}/>
         <BusinessList businesses={businesses} />
       </div>
     );
