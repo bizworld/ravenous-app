a5cf053fb5eec358f14ee00c6d799131aa6a4da9
bizworld <bizworld@outlook.com>
2017-12-21 20:10:18 -0500

    Add credentials to the Yelp.js file.

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,2 +1,4 @@
 /* So far, Ravenous is mostly static. (Build the functionality of) clicking
 on the "Let's Go" button to retrieve results from the Yelp API. */
+// Add credentials (API key from the Yelp API)
+const apiKey = 'U3HjMMElPJpzzjF9h-o2TDYsggerqqc9hEcPXJExyuDYoPE7E4IHL7vIQ9T-kJQzohRXW2jj0GqOJc192Vk86Yt-Wb4HEbv-6TvtCrjQzX-inWTCqZeSVD0iaVY8WnYx';
