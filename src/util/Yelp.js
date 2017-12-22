f135f3885f2e9d13754e3b25f9945e56978d659f
bizworld <bizworld@outlook.com>
2017-12-21 20:54:24 -0500

    Create a Yelp module in the Yelp.js file.

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -2,3 +2,8 @@
 on the "Let's Go" button to retrieve results from the Yelp API. */
 // Add credentials (API key from the Yelp API)
 const apiKey = 'U3HjMMElPJpzzjF9h-o2TDYsggerqqc9hEcPXJExyuDYoPE7E4IHL7vIQ9T-kJQzohRXW2jj0GqOJc192Vk86Yt-Wb4HEbv-6TvtCrjQzX-inWTCqZeSVD0iaVY8WnYx';
+
+/* Create a Yelp module
+by 1st creating an empty object, Yelp, that will store the functionality needed
+to interact with the Yelp API. */
+const Yelp = {};
